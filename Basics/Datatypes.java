ffcec97067b5a10899d64494cc181b812064b77f
4joshi <abhishek._joshi@outlook.com>
2023-02-10 05:15:16 +0530

    Basic datatypes demo file added

---
+++ Basics/Datatypes.java
@@ -0,0 +1,20 @@
+public class Datatypes{
+	public static void main(String[] args) {
+		int intVar = 123;
+		float floatVar = 12.32f; //float should end with 'f'
+		double doubleVar = 12345.43567;
+		byte byteVar = -128;
+		char charVar = 'a';
+		boolean booleanVar = true;
+		
+		
+		//Display the variable values
+		System.out.println("Values for variables : \n");
+		System.out.println("intVar = " + intVar);
+		System.out.println("floatVar = " + floatVar);
+		System.out.println("doubleVar = " + doubleVar);
+		System.out.println("byteVar = " + byteVar);
+		System.out.println("charVar = " + charVar);
+		System.out.println("booleanVar = " + booleanVar);
+	}
+}
